1b9bc3667f1d1e631f322b18ec8c4a049c6d7760
jemmie2025 <jemimahgodswill@gmail.com>
2025-06-17 23:21:42 +0100

    first commit

---
+++ Script.js
@@ -0,0 +1,22 @@
+// CALCULATOR
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+       display.value += input;
+}
+
+function clearDisplay(){
+      display.value
+
+
+
+
+
+function calculate(){
+
+}
+
+
+
+
